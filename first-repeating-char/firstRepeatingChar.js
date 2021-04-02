302bc5029ce0f97fb199f1eda4d104620d27da55
jv <forwardslash.development@gmail.com>
2021-04-02 10:15:19 -0500

    solved challenge

--- first-repeating-char/firstRepeatingChar.js
+++ first-repeating-char/firstRepeatingChar.js
@@ -1,1 +1,12 @@
 // Given a string str, create a function that returns the first repeating character (the first character that we have seen before).  If such a character doesn't exist, return the null character '\0'
+
+function firstRepeatingChar(str) {
+  let visited = {};
+  for (ch of str) {
+    if (visited[ch]) return ch;
+    else visited[ch] = true;
+  }
+  return '\0';
+}
+
+console.log(firstRepeatingChar('moon'));
